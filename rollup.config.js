9ef49e0957860c61f02c0b3838359878152d1c88
christophe le drean <christophe@rouge-rose.net>
2023-10-12 11:51:59 +0200

    Fix erreur js :
    Lors de  la minification du js en 2 fichiers séparés (1 fichier principal
    et 1 fichier carousel), une constante est déclarée deux fois.
    Le js est compilé en une seule fois. A corriger ultérieurement

--- rollup.config.js
+++ rollup.config.js
@@ -6,25 +6,40 @@ const output_plugins = [process.env.NODE_ENV === "production" && terser()];
 
 export default [
 	{
-		input: "src/js/index.js",
+		input: {
+			main: "src/js/index.js",
+			// carousel: "src/js/carousel.js",
+		},
 		plugins: [resolve(), commonjs()],
 		output: [
 			{
-				file: "dist/js/clla.js",
+				entryFileNames: "clla_[name].js",
 				format: "es",
 				plugins: output_plugins,
+				dir: "dist/js",
 			},
 		],
 	},
-	{
-		input: "src/js/carousel.js",
-		plugins: [resolve()],
-		output: [
-			{
-				file: "dist/js/clla_carousel.js",
-				format: "es",
-				plugins: output_plugins,
-			},
-		],
-	},
+	// {
+	// 	input: "src/js/index.js",
+	// 	plugins: [resolve(), commonjs()],
+	// 	output: [
+	// 		{
+	// 			file: "dist/js/clla.js",
+	// 			format: "es",
+	// 			plugins: output_plugins,
+	// 		},
+	// 	],
+	// },
+	// {
+	// 	input: "src/js/carousel.js",
+	// 	plugins: [resolve()],
+	// 	output: [
+	// 		{
+	// 			file: "dist/js/clla_carousel.js",
+	// 			format: "es",
+	// 			plugins: output_plugins,
+	// 		},
+	// 	],
+	// },
 ];
