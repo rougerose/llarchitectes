2e40ece9e4c25b8a99440bb7f46af11aba4fe485
christophe le drean <christophe@rouge-rose.net>
2023-11-26 11:22:56 +0100

    feat: back to top

--- dist/js/llarchitectes.js
+++ dist/js/llarchitectes.js
@@ -390,3 +390,16 @@ class BurgerMenu extends HTMLElement {
 if ("customElements" in window) {
 	customElements.define("burger-menu", BurgerMenu);
 }
+
+// Back to top Observer
+// https://m.signalvnoise.com/how-to-back-to-top-button-without-scroll-events/
+// et https://codepen.io/jasonzimdars/pen/LMaRKq
+const scrollTarget = document.querySelector(".back-to-top");
+const Observercallback = (entries, observer) => {
+	entries.forEach((entry) => {
+		entry.target.classList.toggle("is-intersecting", entry.isIntersecting);
+	});
+};
+const observer = new IntersectionObserver(Observercallback, {});
+
+observer.observe(scrollTarget);
--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,16 @@ import BurgerMenu from "./burger-menu";
 if ("customElements" in window) {
 	customElements.define("burger-menu", BurgerMenu);
 }
+
+// Back to top Observer
+// https://m.signalvnoise.com/how-to-back-to-top-button-without-scroll-events/
+// et https://codepen.io/jasonzimdars/pen/LMaRKq
+const scrollTarget = document.querySelector(".back-to-top");
+const Observercallback = (entries, observer) => {
+	entries.forEach((entry) => {
+		entry.target.classList.toggle("is-intersecting", entry.isIntersecting);
+	});
+};
+const observer = new IntersectionObserver(Observercallback, {});
+
+observer.observe(scrollTarget);
