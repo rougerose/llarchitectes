f1e8b281850069e0252aeacfee8d20480192b975
christophe le drean <christophe@rouge-rose.net>
2025-01-30 16:17:11 +0100

    build version 1.0.2

---
+++ dist/js/llarchitectes_cards.js
@@ -0,0 +1,37 @@
+var llarchitectes_cards = (function () {
+	'use strict';
+
+	// Cards : lien sur l'ensemble de l'élément
+	// Source : https://inclusive-components.design/cards/#callstoaction
+	const init = () => {
+		let cards = document.querySelectorAll(".card");
+
+		if (cards.length > 0) {
+			cards.forEach((card) => {
+				card_add_link(card);
+			});
+		}
+	};
+
+	const card_add_link = (card) => {
+		let down,
+			up,
+			link = card.querySelector(".card_title a");
+
+		card.style.cursor = "pointer";
+		card.onmousedown = () => (down = +new Date());
+		card.onmouseup = () => {
+			up = +new Date();
+			if (up - down < 200) {
+				link.click();
+			}
+		};
+	};
+
+	const cards = {
+		init: init,
+	};
+
+	return cards;
+
+})();
